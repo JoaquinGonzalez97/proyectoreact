fe7b085b44bcfa43574fe1bf303c0bbbdd98336a
Joaquin Gonzalez <gonzalezjoaquinezequiel00@gmail.com>
2022-10-16 17:28:50 -0300

    Realizando correcciones

---
+++ src/componentes/itemCount.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+
+export default function ItemCount({ stock, initial, text }) {
+  const [count, setCount] = useState(initial);
+
+  function handleSubstract() {
+    if (count > 1) setCount(count - 1);
+  }
+
+  function handleAdd() {
+    if (count < stock) setCount(count + 1);
+  }
+
+  return (
+    <div>
+      <h2>Realiza tu compra</h2>
+        <button onClick={handleSubstract}>-</button>
+        <strong>{count}</strong>
+        <button onClick={handleAdd}>+</button>
+      <button>{text}</button>
+    </div>
+  );
+}
